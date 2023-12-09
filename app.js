a4e057a100487d0452926174fe22f06b685f5cc9
salmanyaseen2005 <salmanyaseen9090@gmai.com>
2023-12-10 04:22:21 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,98 @@
+// Quesation  02
+
+// var num1=  prompt("Enter Your First Number")
+// var num2 =prompt("Enter Your Second Number")
+
+// if(num1<=num2){ 
+
+//     alert("Equal")
+//  }
+//  else{
+//     alert("Note equal ")
+//  }
+
+
+// Quesation  03
+
+
+// var number=prompt("Enter Your Number")
+
+// if( number>0){
+//     alert("The Entered Number is  Postive")
+// }
+
+// else if(number<0){
+//     alert("The Entered Number is Negative")
+// }
+// else{
+//     alert("The Entered Number is Zero.")
+// }
+
+// Quesation  04
+
+// var vowel =prompt ("Enter Your Vowel Number")
+
+// if(vowel === "a"|| vowel ==="e"|| vowel ==="i" || vowel==="o" || vowel==="u" ){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// Quesation  05
+
+
+// var password =prompt("Enter Your PassWord")
+
+// var comfirepassword =prompt("Enter Your Comfire PassWord")
+// // alert("Enter Your PassWord")
+
+// if(password &&"comfirepassword"){
+//     alert("Correct! The password")
+// }
+// else("Incorrect password")
+
+// Quesation  06
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";}
+// else{
+// greeting = "Good evening";
+// }
+
+
+// var hour = 13;
+// if (hour < 18) {
+// alert("Good day");}
+// else{
+//     alert("Good evening")
+
+    
+// }
+
+// Quesation  07
+
+var hour=prompt("E")
+
+if(hour>= 0.000 && hour<1200){
+    alert("Good Morning")
+
+}
+
+else if(hour >=1200 && hour<1700){
+    alert("Good iftermoon")
+}
+
+else if(hour>=1700 && hour<2100 ){
+    alert("Good evening")
+}
+
+else if(hour>=2100 && hour<=2359){
+       alert("Good Night")
+}
+
+
+
+
